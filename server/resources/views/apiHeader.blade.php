64b1a3ff91545df03582f0cc0b2dc12e50c95752
Dang Anh Vu <danganhvu1998@gmail.com>
2018-05-26 22:34:59 +0900

    POST ERROR

--- server/resources/views/apiHeader.blade.php
+++ server/resources/views/apiHeader.blade.php
@@ -1,14 +1,11 @@
 <?php
 	header('Access-Control-Allow-Origin: *');
-	try{
-		$username = $_POST['username'];
-		$password = $_POST['password'];	
-		$rememberMe = $_POST['rememberMe'];	
-	} catch {
-		$username = $_GET['username'];
-		$password = $_GET['password'];
-		$rememberMe = $_GET['rememberMe'];
-	}
+	//$username = $_GET['username'];
+	//$password = $_GET['password'];
+	//$rememberMe = $_GET['rememberMe'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];	
+	$rememberMe = $_POST['rememberMe']
 	echo "LOL apiHeader.blade.php<br>";
 	echo $username."<br>";
 	echo $password."<br>";
